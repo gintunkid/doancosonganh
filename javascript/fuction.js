80c51e766c3f830d4d7af68c0461ed4e35e8f4c5
gintunkid <nguyentuankiet160804@gmail.com>
2024-01-18 23:09:48 +0700

    do an

---
+++ javascript/fuction.js
@@ -0,0 +1,17 @@
+
+  var video = document.getElementById("video");
+
+  video.addEventListener("ended", function() {
+    // Thêm hiệu ứng chuyển tiếp hoặc ẩn video
+    video.currentTime = 0;
+    video.play();
+  });
+
+  var video = document.getElementById("video");
+
+  video.addEventListener("ended", function() {
+    // Thêm hiệu ứng chuyển tiếp hoặc ẩn video
+    video.currentTime = 0;
+    video.play();
+  });
+  
